d9f385880de4acd3f95a52f990db4d7c20ce0402
Widya Ade Bagus <dewidyabagus@gmail.com>
2023-06-11 20:05:01 +0700

    memisahkan otlp exporter dari main.go

--- main.go
+++ main.go
@@ -9,11 +9,6 @@ import (
 
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
-	"go.opentelemetry.io/otel/propagation"
-	"go.opentelemetry.io/otel/sdk/resource"
-	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 	"go.opentelemetry.io/otel/trace"
 )
@@ -22,49 +17,23 @@ const serviceName = "go-simple-http"
 const serviceVersion = "1.0.1"
 const defAddress = ":8080"
 
-const otelCollectorEndpoint = "172.17.0.1:4318"
-const otelCollectorPath = "/v1/traces"
-
 var tracer trace.Tracer
 
 func main() {
-	otlpResources, err := resource.Merge(
-		resource.Default(),
-		resource.NewWithAttributes(
-			semconv.SchemaURL,
-			semconv.ServiceName(serviceName),
-			semconv.ServiceVersion(serviceVersion),
-			// attribute.String("environment", "development"),
-		),
-	)
+	otlpResources, err := NewOTLPResources(serviceName, serviceVersion)
 	if err != nil {
-		log.Fatalln("Init Resources OTLP:", err.Error())
+		log.Fatalln("Init OTLP Resources:", err.Error())
 	}
 
-	otlpHttpOptions := []otlptracehttp.Option{
-		otlptracehttp.WithInsecure(),
-		otlptracehttp.WithEndpoint(otelCollectorEndpoint),
-		otlptracehttp.WithURLPath(otelCollectorPath),
-		otlptracehttp.WithTimeout(30 * time.Second),
-	}
-	otlpExporter, err := otlptrace.New(context.Background(), otlptracehttp.NewClient(otlpHttpOptions...))
+	otlpExporter, err := SetupOTLPExporter(context.Background(), otlpResources)
 	if err != nil {
-		log.Fatalln("Init OTLP Exporter:", err.Error())
+		log.Fatalln("Setup OTLP Exporter:", err.Error())
 	}
-
-	tracerProvider := sdktrace.NewTracerProvider(
-		sdktrace.WithBatcher(otlpExporter),
-		sdktrace.WithResource(otlpResources),
-		sdktrace.WithSampler(sdktrace.ParentBased(sdktrace.AlwaysSample())),
-	)
-	defer tracerProvider.Shutdown(context.Background())
-
-	otel.SetTracerProvider(tracerProvider)
-	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
+	defer otlpExporter.Shutdown(context.Background())
 
 	tracer = otel.GetTracerProvider().Tracer(
 		"http-tracer",
-		trace.WithInstrumentationVersion("1.0.0"),
+		trace.WithInstrumentationVersion("1.0.2"),
 		trace.WithSchemaURL(semconv.SchemaURL),
 	)
 
@@ -77,10 +46,14 @@ func main() {
 
 func welcomeHandlerFunc() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		_, span := tracer.Start(r.Context(), "welcome handler")
-		defer span.End()
+		_, span1 := tracer.Start(r.Context(), "Process 1")
+		time.Sleep(150 * time.Millisecond)
+		span1.End()
 
-		time.Sleep(200 * time.Millisecond)
+		_, span2 := tracer.Start(r.Context(), "Process 2")
+		time.Sleep(150 * time.Millisecond)
+		span2.End()
+
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprintln(w, "Hello, World! I am instrumented automatically!")
 	}
---
+++ otlp_exporter.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+)
+
+const defOtelCollectorEndpoint = "172.17.0.1:4318"
+const defOtelCollectorPath = "/v1/traces"
+const defOtelPushTimeout = 30 * time.Second
+
+func NewOTLPResources(name, version string) (*resource.Resource, error) {
+	return resource.Merge(
+		resource.Default(),
+		resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceName(serviceName),
+			semconv.ServiceVersion(serviceVersion),
+			// attribute.String("environment", "development"),
+		),
+	)
+}
+
+func SetupOTLPExporter(ctx context.Context, resource *resource.Resource) (*sdktrace.TracerProvider, error) {
+	otlpHttpOptions := []otlptracehttp.Option{
+		otlptracehttp.WithInsecure(),
+		otlptracehttp.WithEndpoint(defOtelCollectorEndpoint),
+		otlptracehttp.WithURLPath(defOtelCollectorPath),
+		otlptracehttp.WithTimeout(defOtelPushTimeout),
+	}
+	otlpExporter, err := otlptrace.New(ctx, otlptracehttp.NewClient(otlpHttpOptions...))
+	if err != nil {
+		return nil, err
+	}
+
+	otlpTraceProvider := sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(otlpExporter),
+		sdktrace.WithResource(resource),
+		sdktrace.WithSampler(sdktrace.ParentBased(sdktrace.AlwaysSample())),
+	)
+	otel.SetTracerProvider(otlpTraceProvider)
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
+
+	return otlpTraceProvider, nil
+}
